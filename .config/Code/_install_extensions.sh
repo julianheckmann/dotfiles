fbb965e41930de3dc88af0f78253fe033ab18cc7
Julian Heckmann <julianheckmann@gmail.com>
2022-07-31 22:16:21 +0200

    feat: :sparkles: Add customize-ui and project-manager vs code extensions

--- .config/Code/_install_extensions.sh
+++ .config/Code/_install_extensions.sh
@@ -8,11 +8,7 @@ antfu.where-am-i
 dbaeumer.vscode-eslint
 eamodio.gitlens
 file-icons.file-icons
-golang.go
-hashicorp.terraform
-mongodb.mongodb-vscode
 ms-azuretools.vscode-docker
-ms-kubernetes-tools.vscode-kubernetes-tools
 ms-vscode-remote.remote-containers
 ms-vscode.makefile-tools
 redhat.vscode-yaml
@@ -21,6 +17,8 @@ vivaxy.vscode-conventional-commits
 Vue.volar
 znck.grammarly
 mads-hartmann.bash-ide-vscode
+iocave.customize-ui
+alefragnani.project-manager
 )
 
 for i in ${packages[@]}; do
